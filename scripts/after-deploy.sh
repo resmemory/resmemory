95e5e800f88ad8c0c42b47a397b7b90189efe116
dayounglee <verdantjuly@kakao.com>
2023-08-23 21:47:22 +0900

    test: CI/CD

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -3,4 +3,9 @@
 cd /home/ubuntu/build
 
 sudo npm install
-sudo pm2 start "npm run start:build"
+sudo pm2 start "npm run start:gate_build"
+sudo pm2 start "npm run start:dis_build"
+sudo pm2 start "npm run start:users_build"
+sudo pm2 start "npm run start:posts_build"
+sudo pm2 start "npm run start:admin_build"
+sudo pm2 start "npm run start:threads_build"
