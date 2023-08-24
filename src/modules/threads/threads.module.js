f433023ce202728f7fef4cc7c7e4ecdcacb08b4f
gajigaji0401 <gajigaji04@naver.com>
2023-08-24 12:24:35 +0900

    feat: threads

--- src/modules/threads/threads.module.js
+++ src/modules/threads/threads.module.js
@@ -8,10 +8,17 @@ class ThreadsModule extends TcpServer {
   constructor() {
     // 부모 클래스 생성자 호출
     super('threads', process.env.THREADS_PORT ? Number(process.env.THREADS_PORT) : 9010, [
+      // 스레드 조회
       'GET/threads',
+
+      // 스레드 작성
       'POST/threads',
-      'PATCH/threads',
+
+      // 스레드 삭제
       'DELETE/threads',
+
+      // 스레드 완전 삭제
+      'DELETE/threads/admin',
     ]);
 
     this.connectToDistributor(process.env.HOST, process.env.DIS_PORT, (data) => {
@@ -23,35 +30,10 @@ class ThreadsModule extends TcpServer {
   // 클라이언트 요청에 따른 비즈니스 로직 호출
   onRead(socket, data) {
     console.log('onRead', socket.remoteAddress, socket.remotePort, data);
-
-    if (data.method === 'GET' && data.uri === '/threads') {
-      // 'GET/threads' 요청 처리
-      onRequest(socket, data.method, data.uri, data.params, data.key, (s, packet) => {
-        console.log(packet);
-        socket.write(JSON.stringify(packet) + '¶');
-      });
-    } else if (data.method === 'POST' && data.uri === '/threads') {
-      // 'POST/threads' 요청 처리
-      onRequest(socket, data.method, data.uri, data.params, data.key, (s, packet) => {
-        console.log(packet);
-        socket.write(JSON.stringify(packet) + '¶');
-      });
-    } else if (data.method === 'PATCH' && data.uri === '/threads') {
-      // 'PATCH/threads' 요청 처리
-      onRequest(socket, data.method, data.uri, data.params, data.key, (s, packet) => {
-        console.log(packet);
-        socket.write(JSON.stringify(packet) + '¶');
-      });
-    } else if (data.method === 'DELETE' && data.uri === '/threads') {
-      // 'DELETE/threads' 요청 처리
-      onRequest(socket, data.method, data.uri, data.params, data.key, (s, packet) => {
-        console.log(packet);
-        socket.write(JSON.stringify(packet) + '¶');
-      });
-    } else {
-      console.log('Invalid request:', data.method, data.uri);
-      socket.write('Invalid request¶');
-    }
+    onRequest(socket, data.method, data.uri, data.params, data.key, (s, packet) => {
+      console.log(packet);
+      socket.write(JSON.stringify(packet) + '¶');
+    });
   }
 }
 
