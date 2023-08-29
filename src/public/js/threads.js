cbf45b3ebf5ab8ae2ee5a8b8ff7b2293232b1e09
gajigaji0401 <gajigaji04@naver.com>
2023-08-29 20:24:57 +0900

    fix: threads.modal

--- src/public/js/threads.js
+++ src/public/js/threads.js
@@ -15,116 +15,97 @@ const code = {
   370: '알 수 없는 오류가 발생하였습니다.',
 };
 
-const ITEMS_PER_PAGE = 10; // 페이지당 표시할 아이템 개수
-let currentPage = 1;
+document.addEventListener('DOMContentLoaded', () => {
+  viewThreads();
+});
 
-document.addEventListener('DOMContentLoaded', async () => {
+// 스레드 삭제 버튼 클릭 시 DELETE 요청
+async function viewThreads() {
   const threadList = document.getElementById('threadList');
-  const paginationContainer = document.getElementById('pagination');
+  const response = await fetch('/api/threads', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const data = await response.json();
 
-  try {
-    const response = await fetch('/api/threads');
-    const data = await response.json();
-
-    if (data.responseData.result) {
-      const threadsHTML = data.responseData.result
-        .map((thread) => {
-          return `<div class="thread" data-thread-id="${thread.id}">
+  if (data.responseData.result) {
+    const threadsHTML = data.responseData.result
+      .map((thread) => {
+        return `<div class="thread" >
                     ${thread.content}, ${thread.createdAt}
                     <div class="button-container">
-                      <button class="delete-button">삭제</button>
+                      <button class="report-button" data-thread-id="${thread.threadId}">신고</button>
+                      <button class="delete-button" onclick="removeThread"(${thread.threadId})">삭제</button>
                     </div>
                   </div>`;
-        })
-        .join('');
-      threadList.innerHTML = threadsHTML;
-
-      // 스레드 삭제 버튼 클릭 시 DELETE 요청
-      document.addEventListener('click', async (event) => {
-        if (event.target.classList.contains('delete-button')) {
-          const threadContainer = event.target.closest('.thread');
-          const threadId = threadContainer.dataset.threadId;
-
-          try {
-            const response = await fetch(`/api/threads/${threadId}`, {
-              method: 'DELETE',
-            });
-
-            if (response.ok) {
-              threadContainer.remove();
-            } else {
-              console.error('Failed to delete thread');
-            }
-          } catch (error) {
-            console.error('Error deleting thread:', error);
-          }
-        }
-      });
-
-      // 페이지네이션 생성
-      generatePagination(data.totalPages, paginationContainer);
-    } else {
-      threadList.innerHTML = '<p>No threads available.</p>';
-    }
-  } catch (error) {
-    console.error('Error fetching threads:', error);
+      })
+      .join('');
+    threadList.innerHTML = threadsHTML;
   }
-});
-
-function generatePagination(totalPages, paginationContainer) {
-  let paginationHTML = '';
-
-  for (let i = 1; i <= totalPages; i++) {
-    paginationHTML += `<a class="page-link" data-page="${i}">${i}</a>`;
-  }
-
-  paginationContainer.innerHTML = paginationHTML;
 }
 
-// 신고 모달 관련 코드
-const openModalButton = document.getElementById('openModal');
-const closeModalButton = document.getElementById('closeModal');
-const modal = document.getElementById('myModal');
-const submitReportButton = document.getElementById('submitReport');
-const reportInput = document.getElementById('reportInput');
-
-openModalButton.addEventListener('click', function () {
-  modal.style.display = 'flex';
-});
-
-closeModalButton.addEventListener('click', function () {
-  modal.style.display = 'none';
-});
-
-// 스레드 신고 버튼 클릭 시
-document.addEventListener('click', async (event) => {
-  if (event.target.classList.contains('report-button')) {
-    const threadId = event.target.dataset.threadId;
-    modal.style.display = 'flex'; // 모달 열기
-
-    // 신고 내용 제출 버튼 클릭 시
-    submitReportButton.addEventListener('click', async () => {
-      const reportContent = reportInput.value;
-
-      try {
-        const response = await fetch(`/api/threads/${threadId}/report`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ reportContent }),
-        });
-
-        if (response.ok) {
-          console.log('Thread reported successfully');
-        } else {
-          console.error('Failed to report thread');
-        }
-      } catch (error) {
-        console.error('Error reporting thread:', error);
-      }
-
-      modal.style.display = 'none'; // 모달 닫기
-    });
+// 스레드 삭제 버튼 클릭 시 DELETE 요청
+async function removeThread(threadId) {
+  const response = await fetch(`./api/threads/${threadId}`, {
+    method: 'DELETE',
+    headers: {
+      Authorization: localStorage.getItem('Authorization'),
+    },
+  });
+  const result = await response.json();
+  console.log(result);
+  if (result.responseData.code == 731) {
+    alert(code[result.responseData.code]);
+    location.reload();
+  } else {
+    alert(code[result.responseData.code]);
   }
-});
+}
+
+// 신고 버튼 클릭 시 모달창 띄우기
+// document.addEventListener('click', (event) => {
+//   if (event.target.classList.contains('report-button')) {
+//     const threadId = event.target.getAttribute('data-thread-id');
+//     openReportModal(threadId);
+//   }
+//   if (event.target.classList.contains('delete-button')) {
+//     const threadId = event.target.getAttribute('data-thread-id');
+//     removeThread(threadId);
+//   }
+// });
+
+// 모달창 열기
+function openReportModal(threadId) {
+  const reportModal = document.getElementById('reportModal');
+  const confirmReportButton = document.getElementById('confirmReport');
+  const closeModalButton = document.getElementById('closeModal');
+  const reportReasonInput = document.getElementById('reportReason');
+
+  confirmReportButton.onclick = () => {
+    const reason = reportReasonInput.value;
+    if (reason.trim() !== '') {
+      // TODO: 스레드를 신고하는 로직 추가 (이유(reason)를 함께 전송)
+      console.log(`스레드 ${threadId}를 신고합니다. 이유: ${reason}`);
+      closeReportModal();
+    } else {
+      alert('신고 이유를 입력해주세요.');
+    }
+  };
+
+  closeModalButton.onclick = closeReportModal;
+
+  reportModal.style.display = 'block';
+}
+
+// 모달창 닫기
+function closeReportModal() {
+  const reportModal = document.getElementById('reportModal');
+  reportModal.style.display = 'none';
+}
+
+// 스레드 삭제 함수와 중복되지 않도록 주의
+async function removeThread(threadId) {
+  // ... 이전 코드 ...
+}
