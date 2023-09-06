d7de55e4c0c0303143bcca8af4f521aa3b85edbc
Kim Joohee <syap1170@gmail.com>
2023-09-06 17:16:27 +0900

    fix: 이미지 업로드시 리사이징되도록 수정

--- src/modules/posts/imageUploader.js
+++ src/modules/posts/imageUploader.js
@@ -1,6 +1,7 @@
 import aws from 'aws-sdk';
 import fs from 'fs';
 import dotenv from 'dotenv';
+import sharp from 'sharp';
 
 dotenv.config();
 
@@ -12,16 +13,28 @@ let s3 = new aws.S3({
 
 async function imageUploader(img) {
   const filename = `${Date.now()}_${img.originalFilename}`;
+  const resizedFilename = `resized+${filename}`;
+
+  await sharp(img.filepath)
+    .resize({
+      width: 600,
+      height: 600,
+      fit: 'contain',
+      background: { r: 255, g: 255, b: 255, alpha: 1 },
+    })
+    .toFile(`${resizedFilename}`);
+
   let uploadParams = { Bucket: process.env.AWS_BUCKET_NAME, Key: filename, Body: '' };
-  let fileStream = fs.createReadStream(img.filepath);
+  let fileStream = fs.createReadStream(`${resizedFilename}`);
   fileStream.on('error', function (err) {
     console.log('File Error', err);
   });
 
   uploadParams.Body = fileStream;
   uploadParams.ContentType = img.mimetype;
-  // deleteFile(img);
   const result = await s3.upload(uploadParams).promise();
+  fs.unlinkSync(`${resizedFilename}`);
+
   return result.Location;
 }
 
