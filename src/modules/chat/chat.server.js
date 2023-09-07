82de9035cdf47627a57f2f44c6b14399bd980eb1
mingyu kim9411 <awer159@daum.net>
2023-09-07 13:00:27 +0900

    feat:채팅방 기능 추가

---
+++ src/modules/chat/chat.server.js
@@ -0,0 +1,81 @@
+import WebSocket from 'ws';
+import { ChatMessage, ChatOpenLog, ChatCloseLog, getTimes } from './db/mongoose';
+
+export default class ChatServer {
+  constructor(port, onMessage) {
+    this.port = port;
+    this.onMessage = onMessage;
+
+    this.server = new WebSocket.Server({ port });
+
+    this.server.on('connection', async (socket, request) => {
+      const nickname = request.url.split('?')[1].split('=')[1];
+      console.log('Client connected:', socket._socket.remoteAddress, socket._socket.remotePort);
+      const chatOpenLog = new ChatOpenLog({
+        nickname: nickname,
+      });
+
+      await chatOpenLog.save();
+      const arrMessage =await getTimes(nickname);
+      for (const messageData of arrMessage) {
+        socket.send(JSON.stringify(messageData));
+      }
+      socket.on('message', async (message) => {
+        try {
+          const closeTime = JSON.parse(message);
+          if (closeTime.type === 'nickname') {
+            const nickname = messageObject.value;
+            const chatCloseLog = new ChatCloseLog({
+              nickname: nickname,
+            });
+            await chatCloseLog.save();
+          }
+
+          const stringMessage = message.toString();
+
+          console.log('Server Received:', stringMessage);
+
+          // 채팅 메시지를 MongoDB에 저장
+          const chatMessage = new ChatMessage({
+            message: stringMessage,
+            timestamp: Date.now(), // Date.now()를 사용하여 현재 시간을 저장
+          });
+
+          await chatMessage.save(); // await를 사용하여 저장 작업을 기다림
+
+          console.log('채팅 메시지가 성공적으로 저장되었습니다.');
+
+          this.onMessage(socket, stringMessage);
+        } catch (err) {
+          console.error('채팅 메시지 저장 오류:', err);
+        }
+      });
+
+      socket.on('close', async () => {
+        const nickname = request.url.split('?')[1].split('=')[1];
+        const chatCloseLog = new ChatCloseLog({
+          nickname: nickname,
+        });
+
+        console.log(
+          'Client disconnected:',
+          socket._socket.remoteAddress,
+          socket._socket.remotePort,
+        );
+        await chatCloseLog.save();
+      });
+
+      socket.on('error', (err) => {
+        console.error('Client error:', err);
+      });
+    });
+  }
+
+  broadcastMessage(message) {
+    this.server.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  }
+}
