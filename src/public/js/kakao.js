13a71b0ef9d3bf1b410a21016af4cb24cc30fa0b
dayounglee <verdantjuly@kakao.com>
2023-09-08 10:02:08 +0900

    fix : 리디렉션 uri 수정

--- src/public/js/kakao.js
+++ src/public/js/kakao.js
@@ -1,6 +1,6 @@
 async function kakaoLogin() {
   await Kakao.init(kakaoJSkey);
   await Kakao.Auth.authorize({
-    redirectUri: 'http://oemoem.shop:8000/api/oauth',
+    redirectUri: 'http://resmemory.shop/api/oauth',
   });
 }
