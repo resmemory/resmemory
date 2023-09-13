76a6e1690cb59788fc4fa01a85725e94a9c3ba95
dayounglee <verdantjuly@kakao.com>
2023-09-14 01:41:42 +0900

    fix : https 배포

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -11,3 +11,4 @@ sudo pm2 start "npm run start:posts_build"
 sudo pm2 start "npm run start:admin_build"
 sudo pm2 start "npm run start:threads_build"
 sudo pm2 start "npm run start:chat_build"
+sudo mkcert resmemory.shop localhost 127.0.0.1 ::1
