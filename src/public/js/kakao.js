dba8f69f18d2c9f13edbcdbf2665dcc476eecd43
dayounglee <verdantjuly@kakao.com>
2023-09-14 10:00:19 +0900

    chore : resmemory ver 1.0

--- src/public/js/kakao.js
+++ src/public/js/kakao.js
@@ -2,7 +2,7 @@ async function kakaoLogin() {
   try {
     await Kakao.init(kakaoJSkey);
     await Kakao.Auth.authorize({
-      redirectUri: 'https://resmemory.shop/api/oauth',
+      redirectUri: 'http://resmemory.shop/api/oauth',
     });
   } catch (err) {
     alert('로그인이 정상적으로 완료되지 않았습니다. 새로고침 이후 다시 시도해 주세요.');
