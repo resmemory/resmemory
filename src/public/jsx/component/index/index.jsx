7dfac0718002db19930b70715c8c22a316c39e5f
baechanyong1 <chanyongbae341@gmail.com>
2023-12-13 20:32:00 +0900

    feat. index.jsx q

--- src/public/jsx/component/index/index.jsx
+++ src/public/jsx/component/index/index.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 부모컴포넌트 from './부모컴포넌트'; // 실제 파일 경로에 따라 수정
+//import 부모컴포넌트 from './부모컴포넌트'; // 실제 파일 경로에 따라 수정
+import PostList  from './board/boardEa'
+import PostsListByRecent from './board/boardRecent';
+import PostsListByViewCount from './board/boardMostPOP';
 
 const App = () => {
   return (
     <div>
       <h1>전체 애플리케이션</h1>
       <부모컴포넌트 />
-      {/* 다른 컴포넌트들을 추가할 수 있습니다. */}
+      {PostList}
+    {PostsListByRecent}
+    {PostsListByViewCount}
     </div>
   );
 };
