2ab490c08096ca3adde51ba5bef0d5f109af9570
Jang Sihun <chocoj1123@naver.com>
2024-01-11 13:21:19 +0900

    feat : 마이페이지 완성

--- src/public/jsx/components/mypage/myinfo.jsx
+++ src/public/jsx/components/mypage/myinfo.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import '../../components/mypage/mypage.css';
+import '../../components/mypage/myinfo.css';
 
 const MyInfo = () => {
   return (
     <div className="myinfo_first">
-      <div className="flex items-center justify-between mb-2">
+      <div className="nicknamebox">
         <p className="myinfo_nickname">닉네임</p>
         <p className="myinfo_edit">수정</p>
-        <span className="myinfo_mynickname">낭만이란배를타고떠나갈거야어디까지써지는</span>
       </div>
+      <span className="myinfo_mynickname">낭만이란배를타고떠나갈거야어디까지써지는</span>
       <div className="myinfo_sign">
         <p className="myinfo_signselect">가입방식</p>
         <p className="myinfo_signmenu">kakao</p>
