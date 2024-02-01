660cce9a8f0a0f155ebd4490b04690b08b71f414
baechanyong1 <chanyongbae341@gmail.com>
2024-02-01 16:56:01 +0900

    fix : annual, post

--- src/public/jsx/components/main/annual.jsx
+++ src/public/jsx/components/main/annual.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import '../css/annual.css';
 
 const Annual = ({ updateBoardData }) => {
@@ -9,59 +9,61 @@ const Annual = ({ updateBoardData }) => {
 
   const handleCategoryClick = useCallback(async (category) => {
     try {
+      if(loading) return
+      
       setSelectedCategory(category);
       setCurrentPage(1);
-
+  
       if (category === '전체') {
-        window.location.href = '/'; 
+        window.location.href = '/';
         return;
       }
-
-      let apiUrl = `./api/posts/view?pageNum=${currentPage}`;
-
-      if (category) {
-        apiUrl += `&category=${extractYear(category)}`;
-      }
-
+  
+      const year = extractYear(category);
+      let apiUrl = `./api/posts?category=${year}&pageNum=1`; // currentPage 대신에 1로 고정
+  
       setLoading(true);
-
+  
       const response = await fetch(apiUrl, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
         },
       });
-
+  
       const data = await response.json();
-
       if (!data.responseData || data.responseData.length === 0) {
         console.warn('반환된 데이터가 없습니다.');
         return;
       }
-
-      updateBoardData(data.responseData);
+      updateBoardData({ data: data.responseData, category:year });
     } catch (error) {
       console.error('데이터를 불러오는 도중 에러 발생:', error);
     } finally {
       setLoading(false);
     }
-  }, [currentPage, updateBoardData]);
+  }, [updateBoardData]);
+  
 
   const extractYear = (category) => {
-    return category.replace('년', '');
+    return category.replace(/[^0-9]/g, ''); // 숫자만 남기고 나머지 문자 제거
   };
-
+  
+  const handleSort = (data) => {
+    // 정렬 기능 추가
+  }
+  
   return (
     <div>
       <ul className="category">
         {categories.map((category) => (
           <button className="category-button" onClick={() => handleCategoryClick(category)}>
             {category}
-            </button>
+          </button>
         ))}
         <div className="sort"></div>
-        <select className='sort-select'>
-        <option value='board'>최신순</option>
+        <select className='sort-select' onChange={handleSort}>
+          <option value='board'>최신순</option>
           <option value='view'>조회순</option>
           <option value='like'>좋아요순</option>
         </select>
