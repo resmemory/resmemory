8c635af69868cb576b57a484010efd345bb7c103
dayounglee <verdantjuly@kakao.com>
2024-02-01 17:27:10 +0900

    fix : conflict

--- src/public/jsx/components/main/annual.jsx
+++ src/public/jsx/components/main/annual.jsx
@@ -7,52 +7,54 @@ const Annual = ({ updateBoardData }) => {
   const [loading, setLoading] = useState(false);
   const categories = category(2020, 2010, 2000, 1990, 1980, 1970);
 
-  const handleCategoryClick = useCallback(async (category) => {
-    try {
-      if(loading) return
-      
-      setSelectedCategory(category);
-      setCurrentPage(1);
-  
-      if (category === '전체') {
-        window.location.href = '/';
-        return;
+  const handleCategoryClick = useCallback(
+    async (category) => {
+      try {
+        if (loading) return;
+
+        setSelectedCategory(category);
+        setCurrentPage(1);
+
+        if (category === '전체') {
+          window.location.href = '/';
+          return;
+        }
+
+        const year = extractYear(category);
+        let apiUrl = `./api/posts?category=${year}&pageNum=1`; // currentPage 대신에 1로 고정
+
+        setLoading(true);
+
+        const response = await fetch(apiUrl, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        const data = await response.json();
+        if (!data.responseData || data.responseData.length === 0) {
+          console.warn('반환된 데이터가 없습니다.');
+          return;
+        }
+        updateBoardData({ data: data.responseData, category: year });
+      } catch (error) {
+        console.error('데이터를 불러오는 도중 에러 발생:', error);
+      } finally {
+        setLoading(false);
       }
-  
-      const year = extractYear(category);
-      let apiUrl = `./api/posts?category=${year}&pageNum=1`; // currentPage 대신에 1로 고정
-  
-      setLoading(true);
-  
-      const response = await fetch(apiUrl, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-  
-      const data = await response.json();
-      if (!data.responseData || data.responseData.length === 0) {
-        console.warn('반환된 데이터가 없습니다.');
-        return;
-      }
-      updateBoardData({ data: data.responseData, category:year });
-    } catch (error) {
-      console.error('데이터를 불러오는 도중 에러 발생:', error);
-    } finally {
-      setLoading(false);
-    }
-  }, [updateBoardData]);
-  
+    },
+    [updateBoardData],
+  );
 
   const extractYear = (category) => {
     return category.replace(/[^0-9]/g, ''); // 숫자만 남기고 나머지 문자 제거
   };
-  
+
   const handleSort = (data) => {
     // 정렬 기능 추가
-  }
-  
+  };
+
   return (
     <div>
       <ul className="category">
@@ -62,10 +64,10 @@ const Annual = ({ updateBoardData }) => {
           </button>
         ))}
         <div className="sort"></div>
-        <select className='sort-select' onChange={handleSort}>
-          <option value='board'>최신순</option>
-          <option value='view'>조회순</option>
-          <option value='like'>좋아요순</option>
+        <select className="sort-select" onChange={handleSort}>
+          <option value="board">최신순</option>
+          <option value="view">조회순</option>
+          <option value="like">좋아요순</option>
         </select>
       </ul>
     </div>
--- src/public/jsx/components/main/board.jsx
+++ src/public/jsx/components/main/board.jsx
@@ -2,32 +2,47 @@ import React, { useEffect, useState, useRef } from 'react';
 import Masonry from 'react-masonry-css';
 import '../css/board.css';
 
-const Board = ({ initialData }) => {
+const Board = ({ initialData, category }) => {
   const [board, setBoard] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [loading, setLoading] = useState(false);
   const [loadingMore, setLoadingMore] = useState(false);
   const [hasMoreData, setHasMoreData] = useState(true);
-  const [observeSentinel, setObserveSentinel] = useState(true); // 새로운 상태 추가
+  const [observeSentinel, setObserveSentinel] = useState(true);
   const containerRef = useRef(null);
   const sentinelRef = useRef(null);
   const itemsPerPage = 12;
   const itemsPerRow = 5;
 
-  const fetchData = async (page, perPage) => {
+  // 카테고리 변경 시 page가 3부터 시작
+  // 카테고리 변경 시 무한 스크롤 적용 안됌
+
+  useEffect(() => {
+    setBoard(Array.isArray(initialData) ? initialData : []);
+    setCurrentPage(1);
+    setObserveSentinel(true);
+    // console.log("Category changed. currentPage:", currentPage);
+  }, [initialData, category]);
+
+  const fetchData = async (page, perPage, category) => {
     try {
       setLoading(true);
-      const response = await fetch(`./api/posts/?pageNum=${page}&perPage=${perPage}`, {
+      const endpoint = category
+        ? `./api/posts?category=${category}&pageNum=${page}`
+        : `./api/posts/?pageNum=${page}&perPage=${perPage}`;
+      console.log('endpoint', endpoint);
+      const response = await fetch(endpoint, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
         },
       });
+
       const responseData = await response.json();
-      console.log("Fetched data:", responseData);
+      console.log('Fetched data:', responseData);
       return responseData.responseData;
     } catch (error) {
-      console.error('게시판 데이터를 불러오는 중 오류 발생:', error);
+      console.error('Error fetching data:', error);
       return [];
     } finally {
       setLoading(false);
@@ -42,46 +57,55 @@ const Board = ({ initialData }) => {
 
   const loadMoreData = async () => {
     if (!loadingMore && hasMoreData) {
-      setLoadingMore(true); // 로딩 상태로 변경
-  
-      const data = await fetchData(currentPage, itemsPerPage);
-  
-      if (data.length > 0) {
-        setBoard((prevBoard) => [...prevBoard, ...data]);
-        setCurrentPage((prevPage) => prevPage + 1);
-        setHasMoreData(data.length >= itemsPerPage);
-      } else {
-        setHasMoreData(false);
-        setObserveSentinel(false);
+      setLoadingMore(true);
+
+      try {
+        // 센티널 엘리먼트의 높이를 변경
+        sentinelRef.current.style.height = '1000px';
+
+        const data = await fetchData(currentPage, itemsPerPage, category);
+
+        console.log('Fetched data:', data);
+
+        if (data.length > 0) {
+          setBoard((prevBoard) => [...prevBoard, ...data]);
+          setCurrentPage((prevPage) => prevPage + 1);
+          setHasMoreData(data.length >= itemsPerPage);
+        } else {
+          setHasMoreData(false);
+          setObserveSentinel(false);
+        }
+      } catch (error) {
+        console.error('Error loading more data:', error);
+      } finally {
+        setLoadingMore(false);
+
+        // 로딩이 끝나면 센티널 엘리먼트의 높이를 원래대로 복원
+        sentinelRef.current.style.height = '0';
       }
-  
-      setLoadingMore(false); // 로딩 상태 해제
     }
   };
-  
+
   const handleIntersection = (entries, observer) => {
     const { current: sentinel } = sentinelRef;
-  
+    // console.log("Intersection entries:", entries);
+
     entries.forEach((entry) => {
-      console.log("Intersection observed:", entry.isIntersecting);
+      // console.log("Intersection observed:", entry.isIntersecting);
+
       if (entry.isIntersecting && !loading && observeSentinel) {
-        observer.unobserve(sentinel);
-  
-        // 이미 로딩 중이거나 observeSentinel이 false일 때는 추가 호출을 막음
-        if (!loading && observeSentinel) {
-          setLoading(true);
-          console.log("Load more data...");
-          loadMoreData();
-          setLoading(false);
-          // observeSentinel이 여전히 true일 때만 다시 observe를 시작
-          if (observeSentinel) {
-            observer.observe(sentinel);
-          }
+        setLoading(true);
+        console.log('Load more data...');
+        loadMoreData();
+
+        // observeSentinel이 여전히 true일 때만 다시 observe를 시작
+        if (observeSentinel) {
+          observer.observe(sentinel);
         }
       }
     });
   };
-  
+
   useEffect(() => {
     if (hasMoreData && observeSentinel) {
       const { current: sentinel } = sentinelRef;
@@ -90,31 +114,34 @@ const Board = ({ initialData }) => {
         rootMargin: '0px',
         threshold: 1.0,
       });
-  
+
       observer.observe(sentinel);
-  
+
       return () => {
         observer.disconnect();
       };
     }
-  }, [hasMoreData, sentinelRef, handleIntersection, observeSentinel]);
-  
+    setObserveSentinel(true);
+  }, [hasMoreData, sentinelRef, handleIntersection, observeSentinel, category, initialData]);
+
   useEffect(() => {
-    // 초기 로딩 시 한 번만 데이터를 불러옴
+    console.log('useEffect triggered with category:', category);
     if (hasMoreData && observeSentinel) {
+      console.log('Calling category:', category);
+      setCurrentPage(1);
+      console.log('currentPage:', currentPage);
       loadMoreData();
     }
-  }, [observeSentinel]);
-  const breakpointColumnsObj = {
-    default: itemsPerRow,
-    1100: 3,
-    700: 2,
-    500: 1,
-  };
+  }, [observeSentinel, hasMoreData, category]);
 
   return (
     <Masonry
-      breakpointCols={breakpointColumnsObj}
+      breakpointCols={{
+        default: itemsPerRow,
+        1100: 3,
+        700: 2,
+        500: 1,
+      }}
       className="my-masonry-grid"
       columnClassName="my-masonry-grid_column"
       ref={containerRef}
@@ -126,8 +153,9 @@ const Board = ({ initialData }) => {
           <h5 className="post-title">{post.title}</h5>
           <p className="post-content">{post.content}</p>
           <div>
-            <span>${new Date(post.createdAt).toLocaleDateString('ko-KR', { timeZone: 'Asia/Seoul' })}</span>
-            <span>${post.viewCount}</span>
+            <span>
+              ${new Date(post.createdAt).toLocaleDateString('ko-KR', { timeZone: 'Asia/Seoul' })}
+            </span>
           </div>
         </div>
       ))}
