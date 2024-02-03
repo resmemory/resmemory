802d50ce2b5708a8388bf710b848ba7c3d8baea7
dayounglee <verdantjuly@kakao.com>
2024-02-03 19:35:45 +0900

    fix : chatting socket

--- src/public/jsx/pages/ChatPage.jsx
+++ src/public/jsx/pages/ChatPage.jsx
@@ -31,28 +31,28 @@ function ChatPage() {
         // 닉네임 설정
         setUserNickname(nickname);
 
-        const newSocket = new WebSocket(`ws://13.209.87.137:8001?nickname=${nickname}`);
+        const newSocket = new WebSocket(`ws://127.0.0.1:8001/?nickname=${nickname}`);
 
         // 소켓 설정
         setSocket(newSocket);
-        newSocket.onopen = () => {
-          newSocket.addEventListener('message', async (event) => {
-            const data = event.data;
-            const dataParse = await JSON.parse(data);
-            const message = dataParse.message;
-            const senderNickname = dataParse.nickname;
 
-            // 채팅 데이터를 화면에 추가
-            displayMessage(message, senderNickname);
-          });
-        };
+        newSocket.addEventListener('message', async (event) => {
+          const data = event.data;
+          const dataParse = await JSON.parse(data);
+          const message = dataParse.message;
+          const senderNickname = dataParse.nickname;
+
+          // 채팅 데이터를 화면에 추가
+          displayMessage(message, senderNickname);
+        });
+
         newSocket.addEventListener('close', (event) => {
           console.log('WebSocket 연결이 닫혔습니다.');
           const nicknameMessage = {
             type: 'nickname',
             value: `${nickname}`,
           };
-          newSocket.send(JSON.stringify(nicknameMessage));
+          newSocket.onopen = () => newSocket.send(JSON.stringify(nicknameMessage));
         });
       }
     }
@@ -75,9 +75,9 @@ function ChatPage() {
       if (userNickname == null) {
         return alert('로그인이 필요한 기능입니다.');
       }
-
-      socket.send(JSON.stringify(data)); // 서버로 메시지 전송
-
+      socket.onopen = () => {
+        socket.send(JSON.stringify(data)); // 서버로 메시지 전송
+      };
       // 들어온 메시지 담기
       setMessageInput('');
     }
