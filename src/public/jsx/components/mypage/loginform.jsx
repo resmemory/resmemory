50746dceb9421b144065e2c76ae32054ff3ef2cd
dayounglee <verdantjuly@kakao.com>
2024-02-08 13:10:07 +0900

    fix : 마이페이지 CSS 수정

--- src/public/jsx/components/mypage/loginform.jsx
+++ src/public/jsx/components/mypage/loginform.jsx
@@ -1,17 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Logo from '../svg/logo.jsx';
 
 const LoginForm = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleLogin = async () => {
-    window.location.reload();
-  };
-
   return (
-    <div className="myinfo_first">
-      <p className="myinfo_login">로그인 해주세요</p>
-      <img src="../../../assets/image/thn_logo.png" alt="LogoImg" />
+    <div className="login-form">
+      <p>로그인 해주세요</p>
+      <Logo />
     </div>
   );
 };
